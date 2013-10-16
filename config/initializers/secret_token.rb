392ec3260f2c83864c52eff6579a1cfbaaa54292
Jonan Scheffler <jonanscheffler@gmail.com>
2013-10-15 19:55:18 -0700

    Omnium rerum principia parva sunt.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CascadiaStream::Application.config.secret_key_base = 'f0a98e65ad8c3e0b9fcdde8d3d56d4508175581487df0c2889abbb60f5be7954f70d77eed473fe993726f1e07af6ceb5abef33c1bc2e0fb97765749e996406a0'
