68fa52f97beac356edfa6f1549deaf29ab721321
Jonan Scheffler <jonanscheffler@gmail.com>
2013-10-21 14:19:40 -0700

    Add schedule.
    Add schedule.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 CascadiaStream::Application.routes.draw do
   get '/lunch', to: 'welcome#lunch'
   get '/logo', to: 'welcome#logo'
+  get '/schedule', to: 'welcome#schedule'
   root 'welcome#index'
 end
